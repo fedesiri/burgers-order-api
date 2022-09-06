28233574b1c8d5f33b4ebd40041828c15004033c
Federico Siri <94498153+fedesiri@users.noreply.github.com>
2022-09-05 21:35:40 -0300

    Feature: Conexion a la db (#1)
    * agregar consolelog
    
    * Conexion db y creacion de tabla producto
    
    * se eliminan comentarios innecesarios

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const router = require("./routes");
+const { conn } = require("./db");
 
 const app = express();
-const port = process.env.PORT || 3005;
+app.set("port", process.env.PORT || 3005);
 
 // middlewares
 app.use(morgan("dev"));
@@ -14,6 +15,9 @@ app.use(express.json());
 // routes
 app.use("/", router);
 
-app.listen(port, () => {
-    console.log("server on port " + port);
+conn.sync({ force: true }).then(() => {
+    app.listen(app.get("port"), () => {
+        console.log("PostgresDB connected");
+        console.log("Server on port " + app.get("port"));
+    });
 });
---
+++ models/Product.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = sequelize => {
+    sequelize.define("product", {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false,
+        },
+        name: {
+            type: DataTypes.STRING,
+            unique: true,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        price: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        hexcolor: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        status: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+        },
+    });
+};
