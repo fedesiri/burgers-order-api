4ad80f5ea5f61c7e89bd39efaf180141e7b03b50
fedesiri <federicosiri9@gmail.com>
2022-09-12 15:57:52 -0300

    Feature: Creacion ruta post y controllers de order

---
+++ src/controllers/orders.controllers.js
@@ -0,0 +1,43 @@
+const { Order } = require("../db.js");
+const { OrderProduct } = require("../db.js");
+
+const createOrder = async (req, res, next) => {
+    const { name, address, notes, paymentMethod, deliveredBy, takeAway, totalPrice, time, products } = req.body;
+    try {
+        if (!name) {
+            res.send({ success: false, msg: "The order must have a name" });
+        } else if (!address && takeAway === false) {
+            res.send({ success: false, msg: "The order must have an address" });
+        } else if (totalPrice < 0) {
+            res.send({ success: false, msg: "Price must be a number greater than 0." });
+        } else {
+            const order = await Order.create({
+                name,
+                address,
+                notes,
+                paymentMethod,
+                deliveredBy,
+                takeAway,
+                totalPrice,
+                time
+            });
+
+            const promises = products?.map(async productInOrder => {
+                return await OrderProduct.create({
+                    orderId: order.id,
+                    productId: productInOrder.productId,
+                    quantity: productInOrder.quantity,
+                    subtotal: productInOrder.subtotal
+                });
+            });
+            await Promise.all(promises);
+            res.send({ success: true, msg: "Order has been created succesfully!" });
+        }
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports = {
+    createOrder
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const productsRouter = require("./products.routes.js");
+const orderRouter = require("./orders.routes.js");
 
 const router = express.Router();
 
 router.use("/products", productsRouter);
+router.use("/order", orderRouter);
 
 module.exports = router;
