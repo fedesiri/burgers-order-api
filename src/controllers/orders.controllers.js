efad6d1866cc778f75099e4c7558827abac7b079
Federico Siri <94498153+fedesiri@users.noreply.github.com>
2022-09-19 19:21:39 -0300

    Refactor: optimizacion codigo repetido en validaciones order (#19)
    * Refactor: optimizacion codigo repetido en validaciones order
    
    * fix: arreglo de la funcion de validacion para editar una orden

--- src/controllers/orders.controllers.js
+++ src/controllers/orders.controllers.js
@@ -1,14 +1,12 @@
 const { Order, OrderProduct } = require("../db.js");
+const { createOrEditOrderValidationFields, existingOrderValidation, editOrderValidation } = require("./validations/orderValidations");
 
 const createOrder = async (req, res, next) => {
     const { name, address, notes, paymentMethod, deliveredBy, takeAway, totalPrice, time, products } = req.body;
     try {
-        if (!name) {
-            res.send({ success: false, msg: "The order must have a name" });
-        } else if (!takeAway && !address) {
-            res.send({ success: false, msg: "The order must have an address" });
-        } else if (totalPrice < 0) {
-            res.send({ success: false, msg: "Price must be a number greater than 0" });
+        const errorMsg = createOrEditOrderValidationFields(req.body);
+        if (errorMsg) {
+            res.send({ success: false, msg: errorMsg });
         } else {
             const order = await Order.create({
                 name,
@@ -42,9 +40,10 @@ const deleteOrder = async (req, res, next) => {
     const { id } = req.params;
     try {
         const orderToDelete = await Order.findByPk(id);
+        const errorMsg = existingOrderValidation(orderToDelete, id);
 
-        if (!orderToDelete) {
-            res.send({ success: false, msg: `There is no order with the id '${id}'` });
+        if (errorMsg) {
+            res.send({ success: false, msg: errorMsg });
         } else {
             await orderToDelete.destroy();
             res.send({ success: true, msg: `The order was successfully deleted!` });
@@ -58,9 +57,10 @@ const editOrderStatus = async (req, res, next) => {
     const { id } = req.params;
     try {
         const existingOrder = await Order.findByPk(id);
+        const errorMsg = existingOrderValidation(existingOrder, id);
 
-        if (!existingOrder) {
-            res.send({ success: false, msg: `There is no order with the id '${id}'` });
+        if (errorMsg) {
+            res.send({ success: false, msg: errorMsg });
         } else {
             const newStatus = !existingOrder.status;
             await existingOrder.update({ status: newStatus });
@@ -80,9 +80,10 @@ const editOrderDelivery = async (req, res, next) => {
 
     try {
         const existingOrder = await Order.findByPk(id);
+        const errorMsg = existingOrderValidation(existingOrder, id);
 
-        if (!existingOrder) {
-            res.send({ success: false, msg: `There is no order with the id '${id}'` });
+        if (errorMsg) {
+            res.send({ success: false, msg: errorMsg });
         } else {
             await Order.update({ deliveredBy }, { where: { id: id } });
             res.send({ success: true, msg: `Delivery has been changed succesfully to ${deliveredBy}!` });
@@ -97,15 +98,10 @@ const editOrder = async (req, res, next) => {
     const { name, address, notes, paymentMethod, takeAway, totalPrice, time, products } = req.body;
     try {
         const orderToEdit = await Order.findByPk(id);
+        const errorMsg = editOrderValidation(orderToEdit, id, req.body);
 
-        if (!orderToEdit) {
-            res.send({ success: false, msg: `There is no order with the id '${id}'` });
-        } else if (!name) {
-            res.send({ success: false, msg: "The order must have a name" });
-        } else if (!takeAway && !address) {
-            res.send({ success: false, msg: "The order must have an address" });
-        } else if (totalPrice < 0) {
-            res.send({ success: false, msg: "Price must be a number greater than 0" });
+        if (errorMsg) {
+            res.send({ success: false, msg: errorMsg });
         } else {
             orderToEdit.name = name;
             orderToEdit.address = address;
